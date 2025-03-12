fa2f74850b95d6a1b6c70211b72c1a03123bfadb
Abdul Rafay <abdulrafaymehmood123@gmail.com>
2025-03-12 09:45:08 +0000

    i've not done the most yet- 4 topics remaining

--- geeks_site/projectApp/urls.py
+++ geeks_site/projectApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-path('',views.index)
+path('',views.index, name='index'),
+path('simple/',views.simple_response_view, name="response_view"),
+path('user_identification/<int:user_id>', views.user_id, name="user_id")
 ]
--- geeks_site/projectApp/views.py
+++ geeks_site/projectApp/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+user_data = {
+    1: {'name': 'Alice', 'email': 'alice@example.com'},
+    2: {'name': 'Bob', 'email': 'bob@example.com'},
+    3: {'name': 'Charlie', 'email': 'charlie@example.com'},
+}
 
 def index(request):
     return HttpResponse("Hello Geeks this is the app")
+
+def simple_response_view(request):
+    return HttpResponse("This is a simple response.")
+
+def user_id (request, user_id):
+    user = user_data.get(user_id) #get user data based on user_id
+    if user:
+        response = f"User ID: {user_id}<br>"
+        response += f"Name: {user['name']}<br>"
+        response += f"Email: {user['email']}"
+        return HttpResponse(response)
+    else:
+        return HttpResponse("User not found.")
